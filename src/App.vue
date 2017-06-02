f3edc21aacbea4aff781927f90f08eb673a66da1
Medusa1st-PC <littlemeidusha@163.com>
2017-06-02 15:05:15 +0800

    Bug fix in the sequence of clicking the marker

--- src/App.vue
+++ src/App.vue
@@ -16,27 +16,43 @@
 </template>
 
 <script>
+let map, directions_routes, directions_placemarks=[], route_lines=[], route_steps=[];
 const pinGreenSolid = require('./assets/pin-green-solid.png');
 const pinBlueSolid = require('./assets/pin-blue-solid.png');
+const pinRed = require('./assets/pin-red.png');
 export default {
   name: 'app',
   data(){
     return {
-      geocoder : null, 
-      map : null, 
-      marker : null,
-      markersArray : [],
-      searchAddress: '',
+      geocoder: null, 
+      // map: null, 
+      marker: null,
+      markersArray: [],
+      searchAddress: null,
       centerMarkerWidth: 48,
       centerMarkerHeight: 48,
-      calcTop: '',
-      calcLeft: '',
+      calcTop: null,
+      calcLeft: null,     
+      //window onload map center
+      onLoadCenter: {lat: '31.305468476254635', lng: '121.50784492492676'},
+      //navigator start position
+      // navStartPos: {lat: onLoadCenter.lat, lng: onLoadCenter.lng},
+      navStartPos: null,
+      //Gps List
+      parkGPSList: [
+        {lat: '31.308071816223425',lng: '121.5029525756836' },
+        {lat: '31.308035149962773',lng: '121.51102066040039'},
+        {lat: '31.3022783700433'  ,lng: '121.50629997253418'},
+        {lat: '31.304368451830978',lng: '121.51320934295654'}
+        // {lat: '31.305468476254635',lng: '121.50784492492676'}
+      ],
+      userSelectMarker: null,
       directionsService : new qq.maps.DrivingService({
             complete : function(response){
                 let start = response.detail.start,
                     end = response.detail.end;
                 //清除所有路线
-                clearOverlay(route_lines);
+                //clearOverlay(this.route_lines);
                directions_routes = response.detail.routes;
                let routes_desc=[];
                //所有可选路线方案
@@ -112,50 +128,39 @@ export default {
                // let routes=document.getElementById('routes');
                // routes.innerHTML = routes_desc.join('<br>');
             }
-        }),
-      directions_routes : '',
-      directions_placemarks : [],
-      directions_labels : [],
-      start_marker : '',
-      end_marker : '',
-      route_lines : [],
-      step_line : '',
-      route_steps : []
+        })
     }
   },
   mounted: function(){
     // console.log(this);
     this.calcTop = this.$el.clientHeight/2 - this.centerMarkerHeight;
     this.calcLeft= this.$el.clientWidth/2 - this.centerMarkerWidth/2;
-    //Gps List
-    let parkGPSList = [
-    {lat: '31.308071816223425',lng: '121.5029525756836' },
-    {lat: '31.308035149962773',lng: '121.51102066040039'},
-    {lat: '31.3022783700433'  ,lng: '121.50629997253418'},
-    {lat: '31.304368451830978',lng: '121.51320934295654'}
-    // {lat: '31.305468476254635',lng: '121.50784492492676'}
-    ]
+    this.navStartPos = this.onLoadCenter;
 
     //定义map变量 调用 qq.maps.Map() 构造函数   获取地图显示容器
-    this.map = new window.qq.maps.Map(document.getElementById("container"), {
-        center: new window.qq.maps.LatLng(31.305468476254635, 121.50784492492676),      // 地图的中心地理坐标。
+    map = new window.qq.maps.Map(document.getElementById("container"), {
+        center: new window.qq.maps.LatLng(this.onLoadCenter.lat, this.onLoadCenter.lng),      // 地图的中心地理坐标。
         zoom:15                                              // 地图的中心地理坐标。
     });
     //地址和经纬度之间进行转换服务
     //经度 Longitude
     //纬度 Latitude
     this.geocoder = new qq.maps.Geocoder();
-
-    let listener = qq.maps.event.addListener(this.map,'click',
+    this.addUserGPSMarker(this.onLoadCenter.lat, this.onLoadCenter.lng);
+    this.addUserSelectMarker(this.navStartPos.lat, this.navStartPos.lng);
+    for (let i = 0; i < this.parkGPSList.length; i++) {
+      this.addTargetMarker(this.parkGPSList[i].lat, this.parkGPSList[i].lng);
+    };
+    /*let getLocationEvent = qq.maps.event.addListener(map,'click',
       function(event) {
           alert('您点击的位置为:[' + '纬度' + event.latLng.getLat() + ','
                                    + '经度' + event.latLng.getLng() + ']');
       }
-    );
-    this.addUserMarker(31.305468476254635,121.50784492492676);
-    for (let i = 0; i < parkGPSList.length; i++) {
-      this.addTargetMarker(parkGPSList[i].lat, parkGPSList[i].lng);
-    };
+    );*/
+    let _this = this;
+    let centerChangedEvent = qq.maps.event.addListener(map, 'center_changed', function(event) {
+        _this.userSelectMarker.setPosition(event.target.center);
+    });
   },
   methods:{
     codeAddress : function(){
@@ -164,12 +169,12 @@ export default {
         this.geocoder.getLocation("上海"+address);
         //设置服务请求成功的回调函数
         this.geocoder.setComplete(function(result) {
-            this.map.setCenter(result.detail.location);
+            map.setCenter(result.detail.location);
             let marker = new qq.maps.Marker({
-                map: this.map,
+                map: map,
                 position: result.detail.location
             });
-            this.map.zoomTo(14);
+            map.zoomTo(14);
             //点击Marker会弹出反查结果
             qq.maps.event.addListener(marker, 'click', function() {
                 alert("坐标地址为： " + result.detail.location);
@@ -181,13 +186,13 @@ export default {
         });
     },
     getCenter : function(){
-      alert("latlng:" + this.map.getCenter());
+      alert("latlng:" + map.getCenter());
     },
     getCenterTop : function(){
       return "100px";
     },
     //添加标记
-    addUserMarker : function(lat,lng){
+    addUserGPSMarker : function(lat,lng){
         let size = new qq.maps.Size(24, 33),
             target_icon = new qq.maps.MarkerImage(
               pinGreenSolid,
@@ -198,48 +203,72 @@ export default {
         let marker = new qq.maps.Marker({
             icon: target_icon,
             position: location,
-            map: this.map,
+            map: map,
             animation: qq.maps.MarkerAnimation.BOUNCE
         });
     },
-    addTargetMarker : function(lat,lng) {
-        let size = new qq.maps.Size(24, 33),
+    addUserSelectMarker : function(lat,lng){
+        let size = new qq.maps.Size(48, 48),
             target_icon = new qq.maps.MarkerImage(
-              pinBlueSolid,
+              pinRed,
               size,
               new qq.maps.Point(0, 0),
               );
         let location = new qq.maps.LatLng(lat, lng);
-        let marker = new qq.maps.Marker({
+        this.userSelectMarker = new qq.maps.Marker({
             icon: target_icon,
             position: location,
-            map: this.map
+            map: map
         });
-        this.markersArray.push(marker);
-        let markerIndex = this.markersArray.length;
-        qq.maps.event.addListener(marker, 'click', function() {
-            calcRoute(lat,lng);
-        });
+    },
+    addTargetMarker : function(lat,lng) {
+      let size = new qq.maps.Size(24, 33),
+          target_icon = new qq.maps.MarkerImage(
+            pinBlueSolid,
+            size,
+            new qq.maps.Point(0, 0),
+            );
+      let location = new qq.maps.LatLng(lat, lng);
+      let marker = new qq.maps.Marker({
+          icon: target_icon,
+          position: location,
+          map: map
+      });
+      this.markersArray.push(marker);
+      let markerIndex = this.markersArray.length;
+      let _this = this;
+      qq.maps.event.addListener(marker, 'click', function() {
+          _this.calcRoute(lat,lng);
+      });
     },
     //清除地图上的marker
     clearOverlay : function(overlays){
-        let overlay;
-        while(overlay = overlays.pop()){
-            overlay.setMap(null);
-        }
+      let overlay;
+      while(overlay = overlays.pop()){
+          overlay.setMap(null);
+      }
     },
     calcRoute : function(lat,lng){
-    let start_lat = this.map.getCenter().lat;
-    let start_lng = this.map.getCenter().lng;
-    let stop_lat  = lat;
-    let stop_lng  = lng;
-    let policy = 'LEAST_TIME'; //LEAST_DISTANCE,AVOID_HIGHWAYS,REAL_TRAFFIC,PREDICT_TRAFFIC
-    route_steps = [];
+      // let start_lat = this.userSelectMarker.lat;
+      // let start_lng = this.userSelectMarker.lng;
+      // let stop_lat  = lat;
+      // let stop_lng  = lng;
 
-    directionsService.setLocation("上海");
-    directionsService.setPolicy(qq.maps.DrivingPolicy[policy]);
-    directionsService.search(new qq.maps.LatLng(start_lat, start_lng),
-        new qq.maps.LatLng(stop_lat, stop_lng));
+      //清除所有路线
+      this.clearOverlay(route_lines);
+
+      var start_lat = map.getCenter().lat;
+      var start_lng = map.getCenter().lng;
+      var stop_lat  = lat;
+      var stop_lng  = lng;
+
+      let policy = 'LEAST_TIME'; //LEAST_DISTANCE,AVOID_HIGHWAYS,REAL_TRAFFIC,PREDICT_TRAFFIC
+      route_steps = [];
+
+      this.directionsService.setLocation("上海");
+      this.directionsService.setPolicy(qq.maps.DrivingPolicy[policy]);
+      this.directionsService.search(new qq.maps.LatLng(start_lat, start_lng),
+          new qq.maps.LatLng(stop_lat, stop_lng));
     }
   }
 }
